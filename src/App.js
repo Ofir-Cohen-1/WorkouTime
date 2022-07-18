93d52a824c55c15135fe72739a67e5451be8f39f
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-07-18 21:47:47 +0300

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import "./App.css";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Box>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return(
+  <Stack>
+    <Link to="/">
+      <img src={Logo} alt="logo" />
+    </Link>
+
+    <Stack>
+      <Link to="/">Home</Link>
+      <a href="#exercises">Exercises</a>
+    </Stack>
+  </Stack>
+  );
 };
 
 export default Navbar;
