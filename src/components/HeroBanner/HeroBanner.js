9d0a4f0622fbcdb87dae2ccccde445e3e400dcdd
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-07-19 00:12:48 +0300

    Updated Home page

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Box, Stack, Typography , Button} from "@mui/material";
+import HeroBannerImage from '../../assets/images/banner.png'
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44x", xs: "40px" },
+        }}
+      >
+        Sweat, Smile <br /> and Repeat
+      </Typography>
+
+      <Typography fontWeight="22px" lineHeight="35px" mb={3}>
+        Check out the most effective exercises
+      </Typography>
+
+      <Button variant="contained" color="error" href="#exercises">Explore Exercises</Button>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img"/>
+    </Box>
+  );
 };
 
 export default HeroBanner;
