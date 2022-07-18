eb3a45ec7e09374ed5a95a95e0d5aa22909c6462
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-07-18 23:29:24 +0300

    Added Home structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 import "./App.css";
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
-import Logo from "../assets/images/Logo.png";
+import Logo from "../../assets/images/Logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -9,7 +9,12 @@ const Navbar = () => {
     <Stack
       direction="row"
       justifyContent="space-around"
-      sx={{ gap: { sm: "122px", xs: "40px" }, mt: { sm: "32px", xs: "20px" }, justifyContent: "none"}} px='20px'
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: { sm: "32px", xs: "20px" },
+        justifyContent: "none",
+      }}
+      px="20px"
     >
       <Link to="/">
         <img src={Logo} alt="logo" />
