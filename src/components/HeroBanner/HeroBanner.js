54afc0fb545f417bfc86aae9650a97fc53f5e170
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-07-19 12:02:40 +0300

    Updated Home page

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Box, Stack, Typography , Button} from "@mui/material";
-import HeroBannerImage from '../../assets/images/banner.png'
+import { Box, Stack, Typography, Button } from "@mui/material";
+import HeroBannerImage from "../../assets/images/banner.png";
 
 const HeroBanner = () => {
   return (
@@ -18,6 +18,8 @@ const HeroBanner = () => {
 
       <Typography
         fontWeight={700}
+        mb="23px"
+        mt="30px"
         sx={{
           fontSize: { lg: "44x", xs: "40px" },
         }}
@@ -25,12 +27,32 @@ const HeroBanner = () => {
         Sweat, Smile <br /> and Repeat
       </Typography>
 
-      <Typography fontWeight="22px" lineHeight="35px" mb={3}>
+      <Typography fontWeight="22px" lineHeight="35px" mb={4}>
         Check out the most effective exercises
       </Typography>
 
-      <Button variant="contained" color="error" href="#exercises">Explore Exercises</Button>
-      <img src={HeroBannerImage} alt="banner" className="hero-banner-img"/>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+
+      <Typography
+        fontWeight={600}
+        fontSize="200px"
+        color="#FF2625"
+        sx={{
+          opacity: 0.1,
+          display: { lg: "block", xs: "none" },
+        }}
+      >
+        Exercises
+      </Typography>
+
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   );
 };
