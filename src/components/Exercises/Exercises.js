11fd017ed8b69882affe65263869bf4d44b7f78a
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-07-30 23:32:22 +0300

    Updated Exercises component

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../../utils/FetchData";
 
 const Exercises = () => {
   return <div>Exercises</div>;
