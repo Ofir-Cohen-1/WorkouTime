11c3ab838f0acb6a9d7cf8f079b3b6585dac8766
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-08-03 01:58:39 +0300

    Updated horizontal scrollbar style

--- src/components/SimilarExercises/SimilarExercises.js
+++ src/components/SimilarExercises/SimilarExercises.js
@@ -1,22 +1,36 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Divider, Stack, Typography } from "@mui/material";
 import HorizontalScrollbar from "../HorizontalScrollbar/HorizontalScrollbar";
 import Loader from "../Loader/Loader";
+import "./SimilarExercises.css";
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
-      <Typography variant="h3">
-        exercises that target the same muscle group
-      </Typography>
-
-      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
-        {targetMuscleExercises.length ? (
-          <HorizontalScrollbar data={targetMuscleExercises} />
-        ) : (
-          <Loader />
-        )}
-      </Stack>
+      <Box sx={{mb:"70px"}}>
+        <Typography variant="h3" mb={4}>
+          Exercises that target the same muscle group
+        </Typography>
+        <Stack direction="row" sx={{ p: "4px", position: "relative" }}>
+          {targetMuscleExercises.length ? (
+            <HorizontalScrollbar data={targetMuscleExercises} />
+          ) : (
+            <Loader />
+          )}
+        </Stack>
+      </Box>
+      <Box>
+        <Typography variant="h3" mb={4}>
+          Exercises that use the same equipment
+        </Typography>
+        <Stack direction="row" sx={{ p: "4px", position: "relative" }}>
+          {equipmentExercises.length ? (
+            <HorizontalScrollbar data={equipmentExercises} />
+          ) : (
+            <Loader />
+          )}
+        </Stack>
+      </Box>
     </Box>
   );
 };
