2b116d465d2b3c899cf0f5d40486cfcd61dfee57
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-08-02 13:17:29 +0300

    Added sort by bodyPart to horizontal scrollbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import Exercises from "../components/Exercises/Exercises";
 const Home = () => {
   const [bodyPart, setBodyPart] = useState("all");
   const [exercises, setExercises] = useState([]);
-
+console.log(bodyPart);
   return (
     <Box>
       <HeroBanner />
