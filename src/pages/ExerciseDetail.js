1e0549c69b9acc19e01905f8da024d129e300862
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-08-02 18:24:52 +0300

    Added API calls in exercise detail pages

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/FetchData";
+import Detail from "../components/Detail/Detail";
+import ExerciseVideos from "../components/ExerciseVideos/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises/SimilarExercises";
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
