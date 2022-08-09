b6dd9c784cf04fa10fd96dd969f0aca157d0d590
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-08-09 20:52:08 +0300

    Updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,14 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Logo from "../../assets/images/Logo-1.png";
+import Logo from "../../assets/images/Logo-2.png";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <Box mt="80px">
-      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img
-          src={Logo}
-          alt="Logo"
-          className="logo"
-        />
-
+      <Stack gap="40px" alignItems="flex-end" px="10px">
         <Typography variant="h5" pb="40px" mt="20px">
-          Made by an enthusiastic trainee
+          Made By an Enthusiastic Trainee
         </Typography>
       </Stack>
     </Box>
