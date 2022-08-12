64eb0364a5aa468535a86a84c132d200ecb7103e
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-08-12 17:14:47 +0300

    Updated Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import { InfinitySpin } from "react-loader-spinner";
 
 const Loader = () => {
